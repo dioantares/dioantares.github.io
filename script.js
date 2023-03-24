37f26415893b566507e55cede602babed5d1ab3d
Dio Antares <dioantares@gmail.com>
2023-03-24 09:53:21 +0700

    Update Script

--- script.js
+++ script.js
@@ -5,30 +5,6 @@ function handleSubmit(event) {
     let jsonstring = JSON.stringify(value);
     console.log("JSONstring: " + jsonstring);
     console.log("JSONstring length: " + jsonstring.length);
-    
-    //mengkompress JSON
-    let jsonstringcompressed = LZString.compressToBase64(jsonstring); 
-    console.log("compressed: " + jsonstringcompressed);
-    console.log("compressed length: " + jsonstringcompressed.length);
-    
-    // menentukan jumlah qrcode yang dibutuhkan untuk mencakup seluruh data kompresi JSON 
-    // masing-masing qrcode panjangnya 128
-    let max = Math.ceil(jsonstringcompressed.length / 128); 
-    console.log(max);
-    
-    let qrDiv = document.getElementById("qrcode");
+}
 
-    let i = 1;
-    
-    while(i <= max){
-      //membuat string hasil pecahan untuk qrcode
-      let currStr = jsonstringcompressed.substring(0, 129);
-      jsonstringcompressed = jsonstringcompressed.substring(129);
-      let jsonPecahan = "{seq:"+i+",max:"+ max +","+currStr+"}";
-      console.log(jsonPecahan);
-
-      //membuat qrcode
-        new QRCode(qrDiv, jsonPecahan);
-        i++;
-    }
-  }
+form.addEventListener('submit', handleSubmit);
