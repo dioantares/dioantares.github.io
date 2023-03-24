91999d03494cfae62054a4bdd8f4aedd3b4fc461
Dio Antares <dioantares@gmail.com>
2023-03-24 09:45:29 +0700

    Update Script

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const form = document.querySelector("form"),
-    nextBtn = form.querySelector(".nextBtn"),
-    backBtn = form.querySelector(".backBtn"),
-    allInput = form.querySelectorAll(".first input");
+function handleSubmit(event) {
+    event.preventDefault();
+    let data = new FormData(event.target);
+    let value = Object.fromEntries(data.entries());
+    let jsonstring = JSON.stringify(value);
+    console.log("JSONstring: " + jsonstring);
+    console.log("JSONstring length: " + jsonstring.length);
+    
+    //mengkompress JSON
+    let jsonstringcompressed = LZString.compressToBase64(jsonstring); 
+    console.log("compressed: " + jsonstringcompressed);
+    console.log("compressed length: " + jsonstringcompressed.length);
+    
+    // menentukan jumlah qrcode yang dibutuhkan untuk mencakup seluruh data kompresi JSON 
+    // masing-masing qrcode panjangnya 128
+    let max = Math.ceil(jsonstringcompressed.length / 128); 
+    console.log(max);
+    
+    let qrDiv = document.getElementById("qrcode");
 
+    let i = 1;
+    
+    while(i <= max){
+      //membuat string hasil pecahan untuk qrcode
+      let currStr = jsonstringcompressed.substring(0, 129);
+      jsonstringcompressed = jsonstringcompressed.substring(129);
+      let jsonPecahan = "{seq:"+i+",max:"+ max +","+currStr+"}";
+      console.log(jsonPecahan);
 
-nextBtn.addEventListener("click", () => {
-    allInput.forEach(input => {
-        if (input.value != "") {
-            form.classList.add('secActive');
-        } else {
-            form.classList.remove('secActive');
-        }
-    })
-})
-
-backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+      //membuat qrcode
+        new QRCode(qrDiv, jsonPecahan);
+        i++;
+    }
+  }
